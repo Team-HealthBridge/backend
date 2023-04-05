dd570d3c8cb5b353d139e38682793e34f92a61a7
Brian Silah <unpervertedkid@gmail.com>
2023-04-05 17:10:22 +0300

    HealthBit entity class

---
+++ src/main/java/com/vitalis/backend/entities/HealthBit.java
@@ -0,0 +1,53 @@
+package com.vitalis.backend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "health_bits")
+public class HealthBit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "category")
+    private String category;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "picture")
+    private byte[] picture;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getPicture() {
+        return picture;
+    }
+
+    public void setPicture(byte[] picture) {
+        this.picture = picture;
+    }
+}
