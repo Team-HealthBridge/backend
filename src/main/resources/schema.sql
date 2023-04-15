9661716adc7647e3d6762e6c5cb6cbd384a8066f
Brian Silah <unpervertedkid@gmail.com>
2023-04-15 15:56:48 +0300

    Database Migration v1

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,18 @@
-CREATE TABLE IF NOT EXISTS health_bits (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    category VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    picture BLOB
-    );
+CREATE TABLE IF NOT EXISTS health_bits
+(
+    id integer NOT NULL,
+    category VARCHAR(255)  NOT NULL,
+    description text  NOT NULL,
+    picture_url VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS health_events
+(
+    id integer NOT NULL ,
+    title VARCHAR(255) NOT NULL,
+    description text NOT NULL,
+    picture_url VARCHAR(255) NOT NULL,
+    learn_more_url VARCHAR(255) NOT NULL,
+    start_date date NOT NULL,
+    end_date date NOT NULL
+);
